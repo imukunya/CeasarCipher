757a4031948a637538490183fa567d9608086510
imukunya <64461160+imukunya@users.noreply.github.com>
2020-12-18 06:38:29 +0300

    Cipher class and CipherTest class

---
+++ src/test/java/cipher/CipherTest.java
@@ -0,0 +1,13 @@
+package cipher;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class CipherTest extends TestCase {
+
+    @Test
+    public void convertToChars_ConvertingStringToCharacters_Char(){
+
+    }
+
+}
