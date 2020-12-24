ff6e2ff225ab1ece687e1aad411e3699d204745a
Ian Mukunya <imukunya@safaricom.co.ke>
2020-12-24 14:01:54 +0300

    cipher with string builder

---
+++ src/main/java/CeasarCipher.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class CeasarCipher {
+
+    public static void main(String[] args){
+        Console ceaserConsole = System.console();
+        System.out.println("Please enter phrase:");
+        String stringLength = ceaserConsole.readLine();
+
+    }
+}
+
--- src/main/java/cipher/Cipher.java
+++ src/main/java/cipher/Cipher.java
@@ -15,5 +15,25 @@ public class Cipher {
         return this.phrase.toCharArray();
     }
 
+    public String cipherText(String message, int offset){
+
+        StringBuilder result = new StringBuilder();
+        for (char character : message.toCharArray()) {
+            if (character != ' ') {
+                int originalAlphabetPosition = character - 'a';
+                int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
+                char newCharacter = (char) ('a' + newAlphabetPosition);
+                result.append(newCharacter);
+            } else {
+                result.append(character);
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    public String decipherText(String message, int offset) {
+        return cipherText(message, 26 - (offset % 26));
+    }
+
 
 }
