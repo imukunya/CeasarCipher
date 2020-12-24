917e187d5ddf7f9340bd4814596641f5bcf7d197
Ian Mukunya <imukunya@safaricom.co.ke>
2020-12-24 14:08:09 +0300

    decipher and void main function

--- src/main/java/CeasarCipher.java
+++ src/main/java/CeasarCipher.java
@@ -1,11 +1,20 @@
+import cipher.Cipher;
+
 import java.io.Console;
 
 public class CeasarCipher {
 
     public static void main(String[] args){
         Console ceaserConsole = System.console();
+        Cipher ceaser = new Cipher();
+
         System.out.println("Please enter phrase:");
-        String stringLength = ceaserConsole.readLine();
+        String phrase = ceaserConsole.readLine();
+        System.out.print("Converting...");
+        String cipheredText = ceaser.cipherText(phrase,5);
+        System.out.print("your converted text is: "+cipheredText);
+        System.out.print("the original phrase was ________________________________________");
+        System.out.print(phrase);
 
     }
 }
